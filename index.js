eed5e0a22a69c780025389331fa3fae087e77c37
Jiri Peterek <peterek@sourcelab.cz>
2020-03-18 10:55:39 +0100

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true
+            }
+        }
+    }
+}

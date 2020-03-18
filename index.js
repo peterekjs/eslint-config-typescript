ebba1a50efdcdc58c3452484af92f473c5b52e5a
Jiri Peterek <peterek@sourcelab.cz>
2020-03-18 11:14:41 +0100

    add @typescript-eslint plugins

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 module.exports = {
+    extends: [
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+
+    },
     settings: {
+        'import/extensions': [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.json',
+            '.vue'
+        ],
         'import/parsers': {
             '@typescript-eslint/parser': ['.ts', '.tsx'],
+            'babel': ['.js', '.jsx'],
         },
         'import/resolver': {
             typescript: {
